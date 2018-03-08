cdd605d39a68e4cd7ecc7117173a3825d82a93eb
Marc Segarra <marcssegarra@gmail.com>
2018-03-08 11:47:07 +0100

    angle

--- ArkanoidPong/Assets/Scripts/pelota.cs
+++ ArkanoidPong/Assets/Scripts/pelota.cs
@@ -2,21 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pelota : MonoBehaviour {
+public class pelota : MonoBehaviour
+{
 
     public int speed = 20;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         if (Random.value < 0.5f)
             speed = -speed;
         else
             speed = +speed;
 
         gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.right * speed;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketWidth)
+    {
+        return (racketPos.x - ballPos.x) / racketWidth;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+  
+        if (col.gameObject.name == "barra")
+        {
+          
+            float x = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.x);
+
+            Vector2 dir = new Vector2(x, -1).normalized;
+
+         
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+  
+        if (col.gameObject.name == "barra2")
+        {
+      
+            float x = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.x);
+
+            Vector2 dir = new Vector2(x, -1).normalized;
+
+            
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+    }
 }
